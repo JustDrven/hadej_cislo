acff803ec50ce59cdb630f12e98c07221c997a78
JustDrven <petr.nemec11111@gmail.com>
2025-06-22 21:18:04 +0200

    chore(engine): improve console util

--- utils/console.h
+++ utils/console.h
@@ -1,6 +1,13 @@
 #include <cstdlib>
+#include <iostream>
 #include <string>
+
+using namespace std;
 
 void clear_console() {
     system("clear");
+}
+
+void printLine(string str = "") {
+    cout << str << endl;
 }
