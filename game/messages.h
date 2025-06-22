689eacc96145e6b768a736e0a6db5ccc6c6c3d3f
JustDrven <petr.nemec11111@gmail.com>
2025-06-22 15:27:15 +0200

    chore(messages): improve messages manager

--- game/messages.h
+++ game/messages.h
@@ -1,30 +1,32 @@
 #include "../utils/console.h"
 #include "../utils/logger.h"
 #include "../utils/strings.h"
+#include "../game/settings.h"
+#include "../utils/colors.h"
 
 using namespace std;
-
-static string LINE = "=================================";
 
 void send_stats_message(int your_number, int your_guess, int count_of_guesses) {
     clear_console();
 
-    log(LINE);
+    log("");
+    log(GSettings::LINE);
     log("");
     log("");
 
     if (your_number == your_guess)
     {
-        log("     Vyhrál jsi");
+        log(ColorBase::GREEN + "     Vyhrál jsi" + ColorBase::RESET);
         log("Číslo jsi uhádl na " + to_string(count_of_guesses) + ". pokus");
     }
     else
     {
-        log("     Prohrál jsi");
+        log(ColorBase::RED + "     Prohrál jsi" + ColorBase::RESET);
     }
 
     log("");
     log("");
-    log(LINE);
+    log(GSettings::LINE);
+    log("");
     
 }
