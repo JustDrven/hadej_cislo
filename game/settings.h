b51700c3bac1106ba6e84b9d14998b819bd26f28
JustDrven <petr.nemec11111@gmail.com>
2025-06-22 15:26:07 +0200

    feat(game): create game settings

---
+++ game/settings.h
@@ -0,0 +1,12 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <string>
+
+class GSettings {
+    public:
+        inline static int MAX_GUESS = 5;
+        inline static std::string LINE = "============================";
+};
+
+#endif
