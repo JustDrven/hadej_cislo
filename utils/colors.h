6a4d5e5161e1557341671470832e33c90777deef
JustDrven <petr.nemec11111@gmail.com>
2025-06-22 15:26:22 +0200

    feat(design): create color codes

---
+++ utils/colors.h
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+class ColorBase
+{
+    public:
+        inline static string GREEN = "\033[32m";
+        inline static string RED = "\033[31m";
+        inline static string RESET = "\033[37m";
+};
