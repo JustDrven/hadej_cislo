0ef82b04418febea85b761451e7b374a72712820
JustDrven <petr.nemec11111@gmail.com>
2025-06-27 11:31:37 +0200

    chore(engine): fix string utility

--- utils/strings.h
+++ utils/strings.h
@@ -8,12 +8,10 @@ string to_str(int value)
     return to_string(value);
 }
 
-bool isNumber(int& _ci) {
-    int _cv = isdigit(_ci);
-
-    return _cv == 1;
+bool isNumber(int _ci) {
+    return isdigit(_ci) == 0;
 }
 
-bool isDefine(int& _ci) {
+bool isDefine(int _ci) {
     return _ci != -1;
 }
