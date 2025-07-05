afb72e02a453616c5850e01fcfc32a16977141c2
JustDrven <petr.nemec11111@gmail.com>
2025-07-05 19:25:57 +0200

    chore(compile): add stats & strings file into compile bash file

--- cmd/compile.sh
+++ cmd/compile.sh
@@ -3,7 +3,7 @@
 echo [+] Compiler is starting..
 
 file="$1"
-compile_files="main.cpp utils/console.cpp utils/number.cpp src/models/player.cpp src/game.cpp"
+compile_files="main.cpp utils/console.cpp utils/number.cpp utils/strings.cpp src/models/player.cpp src/game.cpp stats/stats.cpp"
 
 if [[ -f "$file" ]]; then
     rm $file
