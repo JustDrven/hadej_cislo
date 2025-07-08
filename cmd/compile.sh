3f23f1fee0c280961bab28a8ee02e70a71ebe8ac
JustDrven <petr.nemec11111@gmail.com>
2025-07-08 08:39:46 +0200

    feat(engine): create build folder

--- cmd/compile.sh
+++ cmd/compile.sh
@@ -3,18 +3,15 @@
 echo [+] Compiler is starting..
 
 file="$1"
-compile_files="main.cpp utils/console.cpp utils/number.cpp utils/strings.cpp src/models/player.cpp src/game.cpp stats/stats.cpp"
+build_folder="build/"
+compile_files="main.cpp utils/console.cpp utils/number.cpp utils/strings.cpp src/models/player.cpp src/game.cpp src/messages.cpp stats/stats.cpp"
 
-if [[ -f "$file" ]]; then
-    rm $file
-
-    if [[ -f "stats.dat" ]]; then
-        rm stats.dat
-    fi
-    
-    echo [-] Old files has been removed!
+if [[ -d $build_folder ]]; then
+    rm -rf $build_folder
 fi
 
-g++ -std=c++17 $compile_files -o $file
+mkdir $build_folder
+
+g++ -std=c++17 $compile_files -o $build_folder/$file
 
 echo [+] Completed!
