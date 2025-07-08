8cfdbea686ca8226329abdf9bf2edeb72607b493
JustDrven <petr.nemec11111@gmail.com>
2025-07-08 08:41:34 +0200

    feat(utils): improve design file

--- utils/number.cpp
+++ utils/number.cpp
@@ -1,11 +1,13 @@
 #include "number.h"
 #include <random>
 
-bool isBigger(int _v1, int _v2)  {
+bool isBigger(int _v1, int _v2)
+{
     return _v1 > _v2;
 }
 
-int getRandom(int _mi, int _ma) {
+int getRandom(int _mi, int _ma)
+{
     static std::random_device rd;
     static std::mt19937 gen(rd());
 
